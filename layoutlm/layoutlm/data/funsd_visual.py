4623886ff93239c996610284900193ed45984af2
yihaoding <81132254+yihaoding@users.noreply.github.com>
2021-03-22 21:06:56 +1100

    Update funsd_visual.py

--- layoutlm/layoutlm/data/funsd_visual.py
+++ layoutlm/layoutlm/data/funsd_visual.py
@@ -9,6 +9,7 @@ logger = logging.getLogger(__name__)
 
 class FunsdDataset(Dataset):
     def __init__(self, args, examples, tokenizer, labels, pad_token_label_id, mode):
+        print(mode)
         if args.local_rank not in [-1, 0] and mode == "train":
             torch.distributed.barrier()  # Make sure only the first process in distributed training process the dataset, and the others will use the cache
 
