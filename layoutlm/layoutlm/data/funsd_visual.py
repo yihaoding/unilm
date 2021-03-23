3825980455b84c65c6b74b256c4eeb4c55f54aac
yihaoding <81132254+yihaoding@users.noreply.github.com>
2021-03-23 15:49:31 +1100

    Update funsd_visual.py

--- layoutlm/layoutlm/data/funsd_visual.py
+++ layoutlm/layoutlm/data/funsd_visual.py
@@ -165,7 +165,11 @@ def convert_examples_to_features(
             # tokens in one bounding box share one visual feature
             visual_features.extend([visual_feature]*len(word_tokens))
             # Use the real label id for the first token of the word, and padding ids for the remaining tokens
-            label_ids.extend([label_map[label]] * len(word_tokens) )
+            label_ids.extend(
+                [label_map[label]] + [pad_token_label_id] * (len(word_tokens) - 1)
+            )
+            
+            
         # Account for [CLS] and [SEP] with "- 2" and with "- 3" for RoBERTa.
         special_tokens_count = 3 if sep_token_extra else 2
         if len(tokens) > max_seq_length - special_tokens_count:
