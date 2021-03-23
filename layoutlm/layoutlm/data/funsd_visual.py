d30ee8c08eac152a9a735bba48d41fab051cbbe4
yihaoding <81132254+yihaoding@users.noreply.github.com>
2021-03-23 16:54:03 +1100

    Update funsd_visual.py

--- layoutlm/layoutlm/data/funsd_visual.py
+++ layoutlm/layoutlm/data/funsd_visual.py
@@ -165,6 +165,7 @@ def convert_examples_to_features(
             # tokens in one bounding box share one visual feature
             visual_features.extend([visual_feature]*len(word_tokens))
             # Use the real label id for the first token of the word, and padding ids for the remaining tokens
+            print(len([label_map[label]]))
             label_ids.extend([label_map[label]])
             label_ids.extend([pad_token_label_id] * (len(word_tokens) - 1))
         print(len(label_ids))
